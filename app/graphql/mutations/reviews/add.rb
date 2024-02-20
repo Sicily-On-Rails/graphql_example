58d58ac63cee88a3e7d8624c936c31faa705691d
Antonino <antoninoscaffidi@gmail.com>
2024-02-20 16:06:36 +0100

    Fixed association repo -> reviews

--- app/graphql/mutations/reviews/add.rb
+++ app/graphql/mutations/reviews/add.rb
@@ -3,6 +3,7 @@ module Mutations
         class Add < BaseMutation
             include Dry::Monads[:result]
             type Types::AddReviewResult, null: false
+            graphql_name "AddReview"
             type Types::ReviewType, null: false
             argument :repo_id, ID, required: true
             argument :rating, Types::ReviewRating, required: true
@@ -15,12 +16,13 @@ module Mutations
                     rating: rating,
                     comment: comment,
                 )
-
+=begin
                 if review.save
                     Success(review)
                 else
                     Failure(review)
                 end
+=end
             end
 
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-  bbelongs_to :repo
+  belongs_to :repo
   belongs_to :user
   validates :rating, presence: true, numericality: {in: 1..5}
   validates :comment, presence: true
